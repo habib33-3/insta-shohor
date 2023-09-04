e7fed6d026e00a63578c32627438f7ce2d6889cc
Habibur Rahman <habib.swi16@gmail.com>
2023-09-04 21:17:45 +0600

    fixed liked

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  likedPostsId.plus(id);
+  likedPostsId.push(id);
   showPosts(posts);
 };
 
@@ -153,6 +153,9 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+
+  document.getElementById("").textContent = "";
+
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
